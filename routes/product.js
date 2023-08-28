9797ed138b8e31386e8786ee947a5a07fa798ecc
Tejas Patil <tejasp094@gmail.com>
2023-08-28 23:10:21 +0530

    some changes

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../controller/product.controller");
 
 router.get("/productlist", userController.getProducts);
-router.get("/productlist/:id", userController.getSingleProductDetails);
+router.get("/product/:id", userController.getSingleProductDetails);
 router.post("/create", userController.addNewProduct);
 
 module.exports = router;
