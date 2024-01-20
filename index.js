71f96d3d4c90317105737e638494e984c6280614
Tejas Patil <tejasp094@gmail.com>
2024-01-20 13:34:08 +0530

    new pkg added for security

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import helmet from "helmet";
 import dotenv from "dotenv";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -9,12 +10,12 @@ const __dirname = dirname(__filename);
 import { createRequire } from "module";
 const requireType = createRequire(import.meta.url)
 const app = express();
+app.use(helmet())
+// Dot env Configuration
 dotenv.config();
-app.use(cors({origin:[process.env.URL]}));
 app.use(bodyParser.json({ limit: "10mb" }));
 const swaggerJson = requireType('./swagger/swagger-output.json')
 import swaggerUi from "swagger-ui-express";
-// Dot env Configuration
 const port = process.env.PORT || "3000";
 // User Rote
 import userRoute from "./routes/user.js";
@@ -29,6 +30,17 @@ dbConnection.dbConnection();
 // Cloudinary Configuration
 configureCloudinary()
 
+// Check if the environment is set to development
+const npmLifecycleEvent = process.env.npm_lifecycle_event
+console.log(npmLifecycleEvent)
+if (npmLifecycleEvent === 'dev') {
+  app.use(cors({origin:[process.env.DEVURL]}));
+// This Route Use For Swagger Docs
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
+}else{
+  app.use(cors({origin:[process.env.PRODURL]}));
+}
+
 // Default Route
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, 'view', 'index', 'index.html'));
@@ -39,14 +51,6 @@ app.use("/api/user", userRoute);
 
 // This Route Use For Manage Product Details
 app.use("/api/product", productRoute);
-
-// Check if the environment is set to development
-const npmLifecycleEvent = process.env.npm_lifecycle_event
-console.log(npmLifecycleEvent)
-if (npmLifecycleEvent === 'dev') {
-// This Route Use For Swagger Docs
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
-} 
 
   // This Route Use For If There Is No Route Found
   app.use("*/", (req, res) => {
